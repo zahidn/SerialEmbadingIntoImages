02aa65ad119b759d3e1fe0137f3c9f086cbdf532
Zahidullah <zahidullah4772@gmail.com>
2021-09-16 18:45:17 +0500

    some changes

--- serial_embed/project/urls.py
+++ serial_embed/project/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import Index
+from .views import *
 
 
 urlpatterns = [
-    path('', Index.as_view(), name="Homepage")
+    path('', Index.as_view(), name="Homepage"),
+    path('login/register', Login.as_view(), name="Login/register")
 ]
